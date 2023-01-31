dcc34d2f97d730bedf088d2ef3f5732b3109410a
Jona Löffler <jona.loeffler@online.de>
2023-01-31 22:17:21 +0100

    Add readme, simplify main function

---
+++ readme.md
@@ -0,0 +1,14 @@
+### Itemis Coding Challenge Problem 3
+
+This solution to Problem 3 is implemented in Rust.
+
+If you have Rust/Cargo installed on your system, execute the code on the
+provided input by running
+
+> cargo run < input
+
+Alternatively, use the provided Dockerfile to build and run on the same input
+data:
+
+> docker build -t itemis .
+> docker run -i itemis < input
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,25 @@
-use clap::Parser;
 use itemis::NumericSystem;
-use std::{error::Error, fs, io, path::PathBuf};
-
-#[derive(Parser)]
-struct Args {
-    /// The file to read
-    input_file: Option<PathBuf>,
-
-    /// Whether to run the interactive console
-    #[arg(short, long)]
-    interactive: bool,
-}
+use std::{
+    error::Error,
+    io::{self, BufRead},
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
-
     let mut system = NumericSystem::new();
 
-    if let Some(file) = args.input_file {
-        let content = fs::read_to_string(&file).expect("Should have been able to read this file");
+    for line in io::stdin().lock().lines() {
+        let line: &str = &line?;
 
-        for line in content.lines() {
-            process(&mut system, line)?;
-        }
-    }
-
-    if args.interactive {
-        println!("\n\n>> Interactive mode, type the next line:");
-
-        let mut buffer = String::new();
-        let stdin = io::stdin();
-        loop {
-            stdin.read_line(&mut buffer)?;
-
-            process(&mut system, &buffer.trim())?;
-
-            buffer = "".to_string();
-        }
-    }
-
-    Ok(())
-}
-
-fn process(system: &mut NumericSystem, line: &str) -> Result<(), Box<dyn Error>> {
-    if line.starts_with("how") || line.ends_with("?") {
-        match system.convert(line) {
-            Ok(result) => println!("{}", result),
-            Err(e) => println!("Error: {}", e),
-        }
-    } else {
-        match system.update(line) {
-            Ok(()) => (),
-            Err(e) => println!("Error: {}", e),
+        if line.trim().to_ascii_lowercase().starts_with("how") || line.trim().ends_with("?") {
+            match system.convert(line.trim()) {
+                Ok(result) => println!("{}", result),
+                Err(e) => println!("Error: {}", e),
+            }
+        } else {
+            match system.update(line.trim()) {
+                Ok(()) => (),
+                Err(e) => println!("Error: {}", e),
+            }
         }
     }
 
